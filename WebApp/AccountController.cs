174665673fc889e808941e1abcaacb75b668a828
Полина Дробченко <polinadrobchenko2560@gmail.com>
2020-10-28 16:29:49 +0300

    'test_task'

---
+++ WebApp/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServiceStack.Host;
+
+namespace WebApp
+{
+    // TODO 4: unauthorized users should receive 401 status code
+    [Route("api/account")]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [Authorize] 
+        [HttpGet]
+        public ValueTask<Account> Get()
+        {
+            //return _accountService.LoadOrCreateAsync(null /* TODO 3: Get user id from cookie */);
+            var acc = _accountService.LoadOrCreateAsync((string)HttpContext.Request.Cookies["id"]);
+            if (acc == null)
+            {
+                throw new HttpException((int)HttpStatusCode.Unauthorized, "Unauthorized");
+            }
+
+            return acc;
+        }
+
+        //TODO 5: Endpoint should works only for users with "Admin" Role
+        [Authorize]
+        [HttpGet("{id}")]
+        public Account GetByInternalId([FromRoute] int id)
+        {
+            return _accountService.GetFromCache(id); 
+        }
+
+        [Authorize]
+        [HttpPost("counter")]
+        public async Task UpdateAccount()
+        {
+            //Update account in cache, don't bother saving to DB, this is not an objective of this task.
+            var account = await Get();
+            account.Counter++;
+        }
+    }
+}
---
+++ WebApp/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    [Route("api")]
+    public class LoginController : Controller
+    {
+        private readonly IAccountDatabase _db;
+
+        public LoginController(IAccountDatabase db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<StatusCodeResult> Login(string userName)
+        {
+            var account = await _db.FindByUserNameAsync(userName);
+            if (account != null)
+            {
+                //TODO 1: Generate auth cookie for user 'userName' with external id
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, userName)
+                };
+
+                ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+            }
+            //TODO 2: return 404 if user not found
+            else
+                return NotFound();
+            return Ok();
+        }
+    }
+}
